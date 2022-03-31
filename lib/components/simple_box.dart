cee7134a9cbf130039140eb58d3d738527758ba5
Sergio <sergiquijano@gmail.com>
2022-03-31 17:18:38 +0200

    feat #7: añado la capacidad de añadir texto debajo de las imagenes

--- lib/components/simple_box.dart
+++ lib/components/simple_box.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 /// [under_image_text] especifica el texto (si queremos) que aparece debajo de la imagen
 /// [callback] define la funcion que se ejecuta cuando clickamos en la caja
 /// [height] y [width] especifican el tamaño de la caja
-GestureDetector create_simple_box(
+Card create_simple_box(
     String image_asset,
     String under_image_text,
     [
@@ -15,18 +15,25 @@ GestureDetector create_simple_box(
     ]
 ){
 
-    return GestureDetector(
-        child: Container(
-          height: height ?? 120.0,
-          width: width ?? 120.0,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(image_asset),
-              fit: BoxFit.fill,
-            ),
-            shape: BoxShape.rectangle,
-          ),
-        ),
-        onTap: callback ?? () => {},
+    return Card(
+        child: Column(
+            children: <Widget>[
+                GestureDetector(
+                    child: Container(
+                      height: height ?? 120.0,
+                      width: width ?? 120.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(image_asset),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                    onTap: callback ?? () => {},
+                ),
+                Text(under_image_text),
+            ]
+        )
     );
 }
