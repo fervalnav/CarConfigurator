2c1013d993ca7a0f6cc0e2e0334d47e8415c93f4
Sergio <sergiquijano@gmail.com>
2022-03-31 19:26:19 +0200

    feat #7: limpio algo el codigo por defecto que traia la vista principal

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -24,17 +24,9 @@ class HomePage extends StatefulWidget {
 
 /// Esta clase representa el estado del widget HomePage
 /// Controla el metodo build() que indica como queremos mostrar la vista
+/// TODO -- creo que lo podemos pasar a StateLess porque de momento la vista no tiene estado como tal,
+/// lo tienen los elementos dentro de la vista
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    // Con esta llamada a setState le indicamos a Flutter que hemos cambiado el estado, y por
-    // tanto debemos cambiar la vista (provocando una llamada a build())
-    // Si cambiamos el valor de _counter sin setState() no se actualizaria la vista
-    setState(() {
-      _counter++;
-    });
-  }
 
   /// Metodo que define la vista que representa esta pagina
   /// Se ejecuta cada vez que llamamos a setState
@@ -43,36 +35,16 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       // Barra horizontal en la parte superior de la app
       appBar: AppBar(
-        // Here we take the value from the HomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // Tomamos el valor de Homepage para usarlo en la barra
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "Configuraciones guardadas:",
+              style: Theme.of(context).textTheme.headline2,
             ),
             create_simple_card(
               "Titulo de pruebas",
@@ -90,9 +62,10 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
 
+      // TODO -- esto tiene que llevar a la vista para realizar la configuracion
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => {},
+        tooltip: 'Crear nueva configuracion',
         child: const Icon(Icons.add),
       ),
     );
