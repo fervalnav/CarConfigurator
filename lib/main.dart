8cede19d1339371ee2d6a92df948f65bdd15a00c
Sergio <sergiquijano@gmail.com>
2022-03-31 20:35:50 +0200

    feat #9: establezco y pruebo el sistema basico de navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'main_view.dart';
+import 'buy_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,20 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CarConfigurator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const HomePage(title: 'Car Configurator'),
+
+      // Especificamos las rutas para la navegacion en la app
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const HomePage(title: "Home"),
+        "/nueva_compra": (context) => const BuyPage(title: "Compra"),
+      },
     );
   }
 }
