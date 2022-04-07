546b3c18ba24cc73c503fce309b48dfecee03c08
Sergio <sergiquijano@gmail.com>
2022-04-07 19:37:51 +0200

    feat #5: añado padding para que se vea mejor la vista

--- lib/views/buy.dart
+++ lib/views/buy.dart
@@ -14,10 +14,8 @@ class BuyPage extends StatefulWidget {
 class _BuyPageState extends State<BuyPage> {
   @override
   Widget build(BuildContext context) {
-
     // Tamaño de las cajas que van a componer esta vista
     var size = 175.0;
-
 
     return Scaffold(
       // Barra horizontal en la parte superior de la app
@@ -26,33 +24,41 @@ class _BuyPageState extends State<BuyPage> {
         title: Text("${widget.title}"),
       ),
       body: Center(
-        child: Column(
 
-          // Para que la columna este "pegada" a la parte superior de la pantalla
-          mainAxisAlignment: MainAxisAlignment.start,
+        // Añado un padding para que haya espacio con la barra superior de la aplicacion
+        child: Padding(
+            padding: EdgeInsets.only(top: 100),
+            child: Column(
 
-          children: <Widget>[
-            Row(
-
-              // Para que las dos imagenes de esta fila esten centradas
-              mainAxisAlignment: MainAxisAlignment.center,
-
-              children: <Widget>[
-                create_simple_box("images/pruebas.jpg", "Modelo", size, size),
-                create_simple_box("images/paleta_colores.jpg", "Color", size, size),
-              ],
-            ),
-            Row(
-              // Para que las dos imagenes de esta fila esten centradas
-              mainAxisAlignment: MainAxisAlignment.center,
+              // Para que la columna este "pegada" a la parte superior de la pantalla
+              // Realmente, estamos pegados a la parte superior del padding que hemos añadido
+              mainAxisAlignment: MainAxisAlignment.start,
 
               children: <Widget>[
-                create_simple_box("images/tapiceria.jpg", "Tapicería", size, size),
-                create_simple_box("images/extras.jpg", "Extras", size, size),
+                Row(
+                  // Para que las dos imagenes de esta fila esten centradas
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: <Widget>[
+                    create_simple_box(
+                        "images/pruebas.jpg", "Modelo", size, size),
+                    create_simple_box(
+                        "images/paleta_colores.jpg", "Color", size, size),
+                  ],
+                ),
+                Row(
+                  // Para que las dos imagenes de esta fila esten centradas
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: <Widget>[
+                    create_simple_box(
+                        "images/tapiceria.jpg", "Tapicería", size, size),
+                    create_simple_box(
+                        "images/extras.jpg", "Extras", size, size),
+                  ],
+                ),
               ],
-            ),
-          ],
-        ),
+            )),
       ),
 
       // TODO -- esto tiene que llevar a la vista para realizar la configuracion
