1acc7bcb354ac64887a6d6601c7be8a83dc26484
Sergio <sergiquijano@gmail.com>
2022-04-15 17:13:55 +0200

    feat #12: añado algo de implementacion al repositorio de configuraciones

--- lib/backend/CarConfigurationRepository.dart
+++ lib/backend/CarConfigurationRepository.dart
@@ -27,19 +27,56 @@ abstract class CarConfigurationRepository{
 /// ni usando el sistema de ficheros para persistir los datos. Esto se hara mas adelante cuando
 /// consumamos una API REST que exponga la persistencia de datos
 class InMemoryCarConfigurationRepository implements CarConfigurationRepository{
+
+  /// Lista que usamos como 'base de datos'
+  /// La inicializamos con algunos valores de pruebas para que al iniciar la aplicacion no aparezca
+  /// todo vacio
+  List<CarConfiguration> list_configurations = [
+    CarConfiguration("Configuracion de pruebas", 200.5),
+    CarConfiguration("Configuracion de pruebas segunda", 100.3141592),
+  ];
+
+
   /// Añadimos una nueva configuracion al repositorio
-  void add_new_car_configuration(CarConfiguration new_config){}
+  void add_new_car_configuration(CarConfiguration new_config){
+
+    // TODO -- habria que comprobar que no existe ya una configuracion en el sistema con el
+    // identificador de esta nueva config
+
+    list_configurations.add(new_config);
+
+  }
 
   /// Buscamos una configuracion en el sistema usando su identificador
-  CarConfiguration? find_car_configurationa(UniqueKey id){}
+  CarConfiguration? find_car_configurationa(UniqueKey id){
+
+    // Buscamos la configuracion en nuestra lista de configuraciones
+    for(config in this.list_configurations){
+      if(config.id == id){
+        return config;
+      }
+    }
+
+    // No hemos encontrado la configuracion buscada
+    return null;
+
+  }
 
   /// Borramos una configuracion del repositorio
-  void delete_car_configuration(UniqueKey id){}
+  void delete_car_configuration(UniqueKey id){
+
+    // TODO -- hay que implementar esta funcionalidad
+
+  }
 
   /// Modificamos una configuracion en el repositorio
   ///
   /// Notar que no pasamos el identificador de la configuracion que queremos modificar. Esto es
   /// porque CarConfiguration tiene ya dicha identificacion como atributo
-  void modify_car_configuration(CarConfiguration new_config){}
+  void modify_car_configuration(CarConfiguration new_config){
+
+    // TODO -- hay que implementar esta funcionalidad
+
+  }
 
 }
