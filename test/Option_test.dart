3667292ad5c2722d5c4a4d6bdb9063dd0ffb97b5
Fernando Valdés Navarro <fernandovaldesnavarro@gmail.com>
2022-04-28 13:08:31 +0200

    option model

---
+++ test/Option_test.dart
@@ -0,0 +1,20 @@
+import 'package:CarConfigurator/models/option.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:CarConfigurator/backend/ActiveConfiguration.dart';
+
+// TODO -- tomo esto de main.dart. Deberia ir en una clase separada
+
+void main() {
+  group("Option tests", () {
+    test("create option", () {
+      Option option = Option(1, "Option", 1.0, "./test.png");
+      expect(option.id, 1);
+      expect(option.text, "Option");
+    });
+
+    test("get Price", () {
+      Option option = Option(1, "Option", 1.0, "./test.png");
+      expect(option.getPrice(), 1.0);
+    });
+  });
+}
